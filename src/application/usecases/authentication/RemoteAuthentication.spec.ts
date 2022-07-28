943aa48fe65a117e0836a7bd70b51e395cee3ee8
Allef Douglas <allefwalker73@gmail.com>
2022-07-28 02:41:47 -0300

    refact: create sut factory

--- src/application/usecases/authentication/RemoteAuthentication.spec.ts
+++ src/application/usecases/authentication/RemoteAuthentication.spec.ts
@@ -1,12 +1,16 @@
 import { HttpClientSpy } from '../../test/MockHttpClient'
 import { RemoteAuthentication } from './RemoteAuthentication'
 
+const makeSut = (url: string = 'any-url') => {
+  const httpClientSpy = new HttpClientSpy()
+  const sut = new RemoteAuthentication(url, httpClientSpy)
+  return { sut, httpClientSpy }
+}
 
 describe("RemoteAuthentication", () => {
   it("should call HttpClient with correct URL", async () => {
-    const url = "any-url"
-    const httpClientSpy = new HttpClientSpy()
-    const sut = new RemoteAuthentication(url, httpClientSpy)
+    const url = "other-url"
+    const { sut, httpClientSpy } = makeSut(url)
     await sut.login()
     expect(httpClientSpy.url).toBe(url)
   })
