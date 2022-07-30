6e9c2a16ca6d8bedf7e8674a24abd854568831c6
Allef Douglas <allefwalker73@gmail.com>
2022-07-30 07:04:21 -0300

    feat: setting up coverage script

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,24 @@ export default {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
-  }
+  },
+  collectCoverageFrom: [
+    'src/application/**/*.ts',
+    'src/domain/**/*.ts',
+    'src/infra/**/*.ts',
+    'src/presentation/components/**/*.{ts,tsx}',
+    'src/presentation/pages/**/*.{ts,tsx}',
+    'src/presentation/store/**/*.{ts,tsx}',
+    '!src/presentation/store/index.ts',
+    '!src/presentation/store/reducers.ts',
+    '!src/infra/http/config/**/*.ts',
+    '!src/infra/http/rtk-query-http-adapter/index.ts',
+    '!src/main/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/tests/**',
+    '!**/coverage/**',
+    '!jest.config.js',
+  ],
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
+
 };
