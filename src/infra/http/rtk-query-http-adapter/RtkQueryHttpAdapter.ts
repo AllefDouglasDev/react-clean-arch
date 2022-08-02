d3db2bc692ce9f45dcadccf7bb5de6635b87d9de
Allef Douglas <allefwalker73@gmail.com>
2022-08-02 18:30:06 -0300

    refact: code style

--- src/infra/http/rtk-query-http-adapter/RtkQueryHttpAdapter.ts
+++ src/infra/http/rtk-query-http-adapter/RtkQueryHttpAdapter.ts
@@ -1,12 +1,17 @@
-import { HttpPostClient, HttpPostClientParams, HttpResponse, HttpStatusCode } from '@/application/protocols/http/HttpClient'
+import { 
+  HttpPostClient, 
+  HttpPostClientParams, 
+  HttpResponse, 
+  HttpStatusCode 
+} from '@/application/protocols/http/HttpClient'
 import { MutationDefinition } from '@reduxjs/toolkit/dist/query'
 import { MutationTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks'
 
 type Error = {
- error?: string
- data?: any 
- status?: number
- originalStatus?: number 
+  error?: string
+  data?: any 
+  status?: number
+  originalStatus?: number 
 }
 
 type RtkQueryHttpAdapterParams = {
@@ -26,7 +31,8 @@ export class RtkQueryHttpAdapter implements HttpPostClient {
     } catch (err) {
       const error = err as Error
       const body = error.data?.error || error.error
-      const statusCode = error.status || error.originalStatus || HttpStatusCode.SERVER_ERROR
+      const statusCode =
+        error.status || error.originalStatus || HttpStatusCode.SERVER_ERROR
       return { body, statusCode }
     }
   }
